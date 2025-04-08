4b1f46710d33a50e433e30b78107dd7ac647a868
Gulshad <azgulj@gmail.com>
2025-04-09 01:09:56 +0300

    Second_task

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
 from flask import Flask, jsonify
+import redis
+import os
 
 app = Flask(__name__)
 
-@app.route('/ping', methods=['GET'])
+#Подключение к Redis (хост 'redis' — имя сервиса из docker-compose)
+redis_host = os.getenv("REDIS_HOST", "redis")
+redis_port = int(os.getenv("REDIS_PORT", 6379))
+r = redis.Redis(host=redis_host, port=redis_port, db=0)
+
+@app.route("/ping")
 def ping():
     return jsonify({"status": "ok"})
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+@app.route("/count")
+def count():
+    visits = r.incr("counter")  #увеличивает значение ключа "counter"
+    return jsonify({"visits": visits})
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
